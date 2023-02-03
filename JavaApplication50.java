1994437d89113cc42a94ed61f07ae7328e14ac31
Giraa02 <118788067+Giraa02@users.noreply.github.com>
2023-02-03 03:30:55 -0800

    Add files via upload

---
+++ JavaApplication50.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication50;
+
+/**
+ *
+ * @author Administrator
+ */
+public class JavaApplication50 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i=1;i<=15;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==8)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+    System.out.println();
+    
+    
+    
+    
+    
+    
+    for (int i=1;i<=4;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if(i==2)
+            {
+                for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==3)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+    System.out.println();
+    
+    
+    
+    
+    
+    
+    for (int i=1;i<=10;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if(i==2)
+            {
+                for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==3)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+    System.out.println();
+    
+    
+    
+    
+    for (int i=1;i<=5;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if(i==2)
+            {
+                for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==3)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==4)
+            {
+                for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            
+        }
+    System.out.println();
+    
+    
+    
+    
+    
+    for (int i=1;i<=15;i++)
+    {
+       System.out.print("*");
+       if (i==1)
+       {
+           for (int j=1;j<=2;j++)
+           {
+               System.out.print(" ");
+           }
+       }
+       else if (i==8)
+       {
+           for (int j=1;j<=2;j++)
+           {
+               System.out.print(" ");
+           }
+       }
+    }
+        // TODO code application logic here
+    }
+    
+}
